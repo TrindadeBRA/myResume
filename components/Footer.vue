75ac415f0d511a1c8c91ed0e5febb4d6d780dc0b
Lucas Trindade <trindadebra@gmail.com>
2023-04-27 19:53:08 -0300

    ajustes nos componentes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,23 +1,35 @@
 <template>
-    <footer class="bg-white">
-      <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-        <div class="flex justify-center space-x-6 md:order-2">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
-            <span class="sr-only">{{ item.name }}</span>
-            <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
-          </a>
+  <footer class="bg-gray-800">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+      <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <div v-for="item in navigation.main" :key="item.name" class="pb-6">
+          <a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-gray-900">{{ item.name }}</a>
         </div>
-        <div class="mt-8 md:order-1 md:mt-0">
-          <p class="text-center text-xs leading-5 text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
-        </div>
+      </nav>
+      <div class="mt-10 flex justify-center space-x-10">
+        <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
+          <span class="sr-only">{{ item.name }}</span>
+          <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+        </a>
       </div>
-    </footer>
-  </template>
-  
-  <script setup>
-  import { defineComponent, h } from 'vue'
-  
-  const navigation = [
+      <p class="mt-10 text-center text-xs leading-5 text-gray-300">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import { defineComponent, h } from 'vue'
+
+const navigation = {
+  main: [
+    { name: 'About', href: '#' },
+    { name: 'Blog', href: '#' },
+    { name: 'Jobs', href: '#' },
+    { name: 'Press', href: '#' },
+    { name: 'Accessibility', href: '#' },
+    { name: 'Partners', href: '#' },
+  ],
+  social: [
     {
       name: 'Facebook',
       href: '#',
@@ -86,5 +98,6 @@
           ]),
       }),
     },
-  ]
-  </script>
+  ],
+}
+</script>
