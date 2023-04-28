bb63ccd92acc7cfbc45fbcc2628259ebb8623f6c
Lucas Trindade <trindadebra@gmail.com>
2023-04-28 01:37:05 -0300

    ajustes front

--- components/Certificate.vue
+++ components/Certificate.vue
@@ -1,109 +1,192 @@
-<template>
+<template @keydown="handleKeyDown">
     <div class="bg-gray-900 py-24 sm:py-20">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto max-w-2xl text-center">
+          <h2 class="text-base font-semibold leading-7 text-indigo-600">Estudos</h2>
           <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Certificados</h2>
           <p class="mt-2 text-lg leading-8 text-gray-300">Learn how to grow your business with our expert advice.</p>
         </div>
-        <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <article v-for="post in posts" :key="post.id" class="flex flex-col items-start justify-between">
-            <div class="relative w-full">
-              <img :src="post.imageUrl" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
+        <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+
+          <article v-for="post in posts" :key="post.id" class="flex flex-col items-start justify-between bg-gray-800 py-5 px-5 rounded-3xl shadow-md shadow-gray-600" >
+            <div class="relative w-full cursor-pointer mb-5" @click="openModal(post.imageUrl)">
+              <img :src="post.imageUrl" alt="" class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2] cursor-pointer"/>
               <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-white/10" />
             </div>
-            <div class="max-w-xl">
-              <div class="mt-8 flex items-center gap-x-4 text-xs">
+            <div class="w-full">
+              <div class="group relative">
+                <h3 class="mt-4 text-lg font-semibold leading-6 text-white group-hover:text-gray-300 text-center cursor-pointer" @click="openModal(post.imageUrl)">
+                  {{ post.title }}
+                </h3>
+                <p class="mt-4 text-base font-light leading-6 text-white group-hover:text-gray-300 text-center cursor-pointer" @click="openModal(post.imageUrl)">
+                  {{ post.title }}
+                </p>
+              </div>
+              <div class="mt-8 flex items-center gap-x-4 text-xs justify-around pb-2">
                 <time :datetime="post.datetime" class="text-gray-300">{{ post.date }}</time>
                 <a :href="post.category.href" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-700 hover:bg-gray-100">{{ post.category.title }}</a>
               </div>
-              <div class="group relative">
-                <h3 class="mt-3 text-lg font-semibold leading-6 text-white group-hover:text-gray-300">
-                  <a :href="post.href">
-                    <span class="absolute inset-0" />
-                    {{ post.title }}
-                  </a>
-                </h3>
-                <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-300">{{ post.description }}</p>
-              </div>
-              <div class="relative mt-8 flex items-center gap-x-4">
-                <img :src="post.author.imageUrl" alt="" class="h-10 w-10 rounded-full bg-gray-100" />
-                <div class="text-sm leading-6">
-                  <p class="font-semibold text-white">
-                    <a :href="post.author.href">
-                      <span class="absolute inset-0" />
-                      {{ post.author.name }}
-                    </a>
-                  </p>
-                  <p class="text-gray-600">{{ post.author.role }}</p>
-                </div>
-              </div>
             </div>
           </article>
+
+          <!-- Modal -->
+          <div v-show="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+            <div class="w-4/5 relative">
+              <img :src="showImageUrl" alt="Imagem" class="max-w-full max-h-full">
+              <button @click="closeModal" class="absolute -top-8 -right-8 m-4 p-2 rounded-full bg-white text-gray-800 shadow-md">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
   </template>
-  
-  <script setup>
-  const posts = [
-    {
-      id: 1,
-      title: 'Boost your conversion rate',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      imageUrl:
-        'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
-      author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    {
-      id: 1,
-      title: 'Boost your conversion rate',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      imageUrl:
-        'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
-      author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    {
-      id: 1,
-      title: 'Boost your conversion rate',
-      href: '#',
-      description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-      imageUrl:
-        'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      category: { title: 'Marketing', href: '#' },
-      author: {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        href: '#',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-    },
-    // More posts...
-  ]
 
+  <script>
+  export default {
+    data() {
+      return {
+        showModal: false,
+        showImageUrl: "",
+        posts: [
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/analise-desenvolvimento-de-sistemas.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/solid-os-5-principios-para-as-boas-praticas-da-poo.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+          {
+            id: 1,
+            title: 'Boost your conversion rate',
+            href: '#',
+            description:
+              'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+            imageUrl:
+              'https://lucastrindade.dev/images/certificados/vue-js-3.webp',
+            date: 'Mar 16, 2023',
+            datetime: '2023-03-16',
+            category: { title: 'Marketing', href: '#' },
+            author: {
+              name: 'Michael Foster',
+              role: 'Co-Founder / CTO',
+              href: '#',
+              imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            },
+          },
+        ],
+      };
+    },
+    methods: {
+      handleKeyDown(event) {
+      if (event.keyCode === 27) {
+        // Pressionada a tecla ESC
+        this.closeModal();
+      }
+    },
+      openModal(imageUrl) {
+        this.showImageUrl = imageUrl
+        this.showModal = true;
+        document.body.classList.add('overflow-hidden');
+      },
+      closeModal() {
+        this.showModal = false;
+        document.body.classList.remove('overflow-hidden');
+      },
+    },
+  };
   </script>
+
--- components/Experiences.vue
+++ components/Experiences.vue
@@ -1,65 +1,152 @@
 <template>
-  <div class="bg-gray-900 py-24 sm:py-32">
+  <div class="bg-gray-800 py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:mx-0">
-        <h2 class="text-base font-semibold leading-7 text-indigo-400">Everything you need</h2>
-        <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No server? No problem.</p>
-        <p class="mt-6 text-lg leading-8 text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis.</p>
+          <div>
+            <h2 class="text-base font-semibold leading-7 text-indigo-600">Minha jornada</h2>
+            <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Experiencias Profissionais</p>
+          </div>
+      <div class="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 dark:border-gray-700 pt-10 sm:mt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3" >
+        <article
+          v-for="experience in professional_experiences"
+          :key="experience.id"
+          class="flex max-w-xl flex-col items-start justify-between bg-gray-900 p-6 rounded-2xl border border-gray-600 cursor-pointer w-full"
+        >
+          <div class="flex items-center gap-x-4 text-xs w-full">
+
+            <div v-if="experience.departure_date == 'current'" class="flex gap-3 items-center w-full justify-between">
+              
+              <p class="text-xs text-gray-500">{{ experience.admission_date }} - {{ experience.departure_date }}</p>
+
+              <span class="flex items-center gap-x-1.5 rounded-md px-3 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-700">
+                <svg class="h-1.5 w-1.5 fill-green-400" viewBox="0 0 6 6" aria-hidden="true">
+                  <circle cx="3" cy="3" r="3" />
+                </svg>
+                online
+              </span>
+
+            </div>
+
+            <div v-if="experience.departure_date !== 'current'" class="flex gap-3 items-center w-full justify-between">
+              
+              <p class="text-xs text-gray-500">{{ experience.admission_date }} - {{ experience.departure_date }}</p>
+
+              <span class="flex items-center gap-x-1.5 rounded-md px-3 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-700">
+                <svg class="h-1.5 w-1.5 fill-red-800" viewBox="0 0 6 6" aria-hidden="true">
+                  <circle cx="3" cy="3" r="3" />
+                </svg>
+                offline
+              </span>
+
+            </div>
+
+          </div>
+          <div class="relative mt-4 flex items-center gap-x-4">
+            <img
+              :src="experience.imageUrl"
+              alt=""
+              class="h-10 w-10 rounded-full bg-gray-50"
+            />
+            <div class="text-sm leading-6">
+              <p class="font-semibold text-white">
+                <a :href="experience.company_website">
+                  <span class="absolute inset-0" />
+                  {{ experience.company_name }}
+                </a>
+              </p>
+              <p class="text-gray-400">{{ experience.role }}</p>
+            </div>
+          </div>
+
+        </article>
       </div>
-      <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
-        <div v-for="feature in features" :key="feature.name" class="relative pl-9">
-          <dt class="inline font-semibold text-white">
-            <component :is="feature.icon" class="absolute left-1 top-1 h-5 w-5 text-indigo-500" aria-hidden="true" />
-            {{ feature.name }}
-          </dt>
-          {{ ' ' }}
-          <dd class="inline">{{ feature.description }}</dd>
-        </div>
-      </dl>
     </div>
   </div>
 </template>
 
-<script setup>
-import {
-  ArrowPathIcon,
-  CloudArrowUpIcon,
-  Cog6ToothIcon,
-  FingerPrintIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from '@heroicons/vue/20/solid'
-
-const features = [
-  {
-    name: 'Push to deploy.',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security.',
-    description: 'Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.',
-    icon: FingerPrintIcon,
-  },
-  {
-    name: 'Powerful API.',
-    description: 'Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.',
-    icon: Cog6ToothIcon,
-  },
-  {
-    name: 'Database backups.',
-    description: 'Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.',
-    icon: ServerIcon,
-  },
-]
+<script>
+  export default {
+    data() {
+      return {
+        professional_experiences: [
+          {
+            id: 1,
+            company_name: "UDS Soluçôes",
+            company_website: "https://uds.com.br",
+            imageUrl: "https://media.licdn.com/dms/image/C4E0BAQGA6GugamGwdg/company-logo_200_200/0/1584452706815?e=1691020800&v=beta&t=QqC6GN5ap1IqAVzMSgHrR9FxtcVTxR8lAtpxF6DMEH8",
+            role: "Desenvolvedor Full-Stack Pleno",
+            admission_date: "2022-03-16",
+            departure_date: "current",
+            activities: [
+              "Stacks: WordPress e Vue.js",
+              "Metodologia ágil: SCRUM",
+              "Desenvolvimento em (WP / PHP / Laravel / Vue.js)",
+              "Desenvolvimento de layouts (Figma)",
+              "Jira / Bitbucket",
+              "Correção de bugs",
+            ],
+          },
+          {
+            id: 2,
+            company_name: "Google",
+            company_website: "https://google.com",
+            imageUrl: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            role: "Software Engineer",
+            admission_date: "2020-01-01",
+            departure_date: "2022-01-01",
+            activities: [
+              "Stacks: WordPress e Vue.js",
+              "Metodologia ágil: SCRUM",
+              "Desenvolvimento em (WP / PHP / Laravel / Vue.js)",
+              "Desenvolvimento de layouts (Figma)",
+              "Jira / Bitbucket",
+              "Correção de bugs",
+            ],
+          },
+          {
+            id: 3,
+            company_name: "Microsoft",
+            company_website: "https://microsoft.com",
+            imageUrl: "https://www.microsoft.com/en-us/research/wp-content/themes/microsoft-research/images/Microsoft-logo_rgb_cropped.png",
+            role: "Software Developer",
+            admission_date: "2018-01-01",
+            departure_date: "2020-01-01",
+            activities: [
+              "Stacks: WordPress e Vue.js",
+              "Metodologia ágil: SCRUM",
+              "Desenvolvimento em (WP / PHP / Laravel / Vue.js)",
+              "Desenvolvimento de layouts (Figma)",
+              "Jira / Bitbucket",
+              "Correção de bugs",
+            ],
+          },
+          {
+            id: 4,
+            company_name: "Amazon",
+            company_website: "https://amazon.com",
+            imageUrl: "https://d1.awsstatic.com/logos/aws-logo-lockups/poweredbyaws/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af1a2e3d7a31f6dc9b7d6488d975bbdd7a6.png",
+            role: "Software Engineer",
+            admission_date: "2016-01-01",
+            departure_date: "2018-01-01",
+            activities: [
+              "Stacks: WordPress e Vue.js",
+              "Metodologia ágil: SCRUM",
+              "Desenvolvimento em (WP / PHP / Laravel / Vue.js)",
+              "Desenvolvimento de layouts (Figma)",
+              "Jira / Bitbucket",
+              "Correção de bugs",
+            ],
+          },
+        ],
+      }
+    },
+    methods: {
+      lastSeenDate(departure_date) {
+        const today = new Date();
+        const lastSeen = new Date(departure_date);
+        const timeDiff = Math.abs(today.getTime() - lastSeen.getTime());
+        const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        return `Experiência encerrada a ${daysDiff} dias...`;
+      }
+    }
+  }
 </script>
